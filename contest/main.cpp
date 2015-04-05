29eb2df0e6ef98e4d1f52315f5db3e3a54a31fe5
andyqee <vitasone@gmail.com>
2015-04-05 16:42:48 +0800

    Initial Commit

---
+++ contest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  contest
+//
+//  Created by andy on 5/4/15.
+//  Copyright (c) 2015 andy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
